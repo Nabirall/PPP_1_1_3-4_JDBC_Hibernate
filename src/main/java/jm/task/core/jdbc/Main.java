b6d24b86c17ef8314051a1cdbdf80192d5a2f1ff
Nabirall <drozdover99@mail.ru>
2023-04-27 21:30:44 +0300

    var28

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,19 @@
 package jm.task.core.jdbc;
 
+
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+
 public class Main {
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
-    }
+public static void main(String[] args) {
+    UserServiceImpl userService = new UserServiceImpl();
+    userService.createUsersTable();
+    userService.saveUser("bob", "lol", (byte) 34);
+    userService.saveUser("vov", "lole", (byte) 4);
+    userService.saveUser("zpd", "lolw", (byte) 124);
+    userService.saveUser("cww", "lolq", (byte) 54);
+    userService.getAllUsers();
+    userService.cleanUsersTable();
+    userService.dropUsersTable();
+}
 }
